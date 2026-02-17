21202ffbd489de21037c9262312e7f8e3a5c8210
rohith reddy <rohithsrlv@gmail.com>
2026-02-16 21:43:52 -0500

    more validation added

--- Welcome.py
+++ Welcome.py
@@ -6,14 +6,19 @@ def employee_details():
         if name.isdigit():
             name = input("Name cannot be Numeric. Please enter your Name: ")
 
-        emp_id = int(input(f"{name}, what is your ID?: "))
+        emp_id_input = input(f"Hey {name}, what is your ID?: ")
+
+        while not emp_id_input.isdigit():
+            emp_id_input = input("Invalid ID. Please enter a numeric value: ")
+
+        emp_id = int(emp_id_input)
 
         if emp_id < 30:
             print(f"Hello {name}, welcome to  dev team")
         else:
             print(f"Hello {name}, Welcome to Ops Team")
     except ValueError:
-        print("Invalid ID. Please enter a numeric value.")
+        print("check the program execution again")
 
 
 employee_details()
